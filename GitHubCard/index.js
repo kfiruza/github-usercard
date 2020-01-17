64cebb2e554252a83a9c75ebaef5f21b52fc78c2
khashimovafiruza@gmail.com <khashimovafiruza@gmail.com>
2020-01-16 21:02:27 -0600

    Completed GitHubCard Project

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,19 @@
            https://api.github.com/users/<your name>
 */
 
+ axios.get('https://api.github.com/users/kfiruza')
+ .then(response => {
+  console.log(response.data);
+
+  const newCard = userCreator(response.data);
+  entryPoint.appendChild(newCard);
+})
+.catch (error => {
+  console.log(error);
+});
+
+
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -24,7 +37,21 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [
+  'https://api.github.com/users/ErikRodriguez-webdev', 
+  'https://api.github.com/users/brandi-jones',
+  'https://api.github.com/users/Devin44G',
+  'https://api.github.com/users/kfiruza'
+];
+
+followersArray.forEach(object => {
+axios.get(object)
+.then(response => {
+  console.log(response.data);
+  const newCard = cardCreator(response.data);
+  entryPoint.appendChild(newCard);
+  })
+})
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -53,3 +80,59 @@ const followersArray = [];
   luishrd
   bigknell
 */
+
+
+  
+
+function cardCreator(profile) {
+
+  const newCard = document.createElement("div"),
+  profImg = document.createElement("img"),
+  userInfo = document.createElement("div"),
+  realName = document.createElement("h3"),
+  handle = document.createElement("p"),
+  location = document.createElement("p"),
+  urlCard = document.createElement("p"),
+  url = document.createElement("a"),
+  followers = document.createElement("p"),
+  following = document.createElement("p"),
+  bio = document.createElement("p");
+
+  // classes
+  newCard.classList.add("card");
+  userInfo.classList.add("card-info");
+  realName.classList.add("name");
+
+
+  //text content
+  newCard.appendChild(profImg);
+  newCard.appendChild(userInfo);
+  userInfo.appendChild(realName);
+  userInfo.appendChild(handle);
+  userInfo.appendChild(location);
+  userInfo.appendChild(urlCard);
+
+  userInfo.appendChild(followers);
+  userInfo.appendChild(following);
+  userInfo.appendChild(bio);
+
+  // append
+   profImg.src = profile.avatar_url;
+  realName.textContent = profile.name;
+  handle.textContent = profile.login;
+  location.textContent = `Location: ${profile.location}`;
+  
+  // Adding Profile text to urlCard and appending the 'a' tag before assigning it the URL
+  urlCard.textContent = `Profile: `;
+  urlCard.appendChild(url);
+  url.textContent = profile.html_url;
+
+  // set text content (continued)
+  followers.textContent = `Followers: ${profile.followers}`;
+  following.textContent = `Following: ${profile.following}`;
+  bio.textContent = `Bio: ${profile.bio}`;
+
+  return newCard;
+}
+
+const entryPoint = document.querySelector(".cards");
